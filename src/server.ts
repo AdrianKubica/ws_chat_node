f877510e386c0939d83941e1c977980379cf27b4
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-05-17 01:32:10 +0200

    Basic client server communication

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import app from './app'
+import http from 'http'
+import socketio from 'socket.io'
 
 const port = process.env.PORT
+const server = http.createServer(app)
+const io = socketio(server)
 
-app.listen(port, () => {
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New websocket connection')
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is running at port ${port}`)
 })
