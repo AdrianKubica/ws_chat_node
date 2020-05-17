454322be95ff95f613b601dde5a83dbb31bd50fc
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-05-17 02:03:06 +0200

    Changing box color on msg

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,24 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated', count)
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
+
+const msgInput = document.querySelector('#msg')
+const redBox = document.querySelector('.red-box')
+
+socket.on('message', (message) => {
+    console.log(message)
+    redBox.style.backgroundColor = `rgb(${parseInt(Math.random() * 255)}, ${parseInt(Math.random() * 255)}, ${parseInt(Math.random() * 255)})`
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
+document.querySelector('form').addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    const chatMsg = msgInput.value
+    socket.emit('chatMsg', chatMsg)
 })
--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,27 @@ const port = process.env.PORT
 const server = http.createServer(app)
 const io = socketio(server)
 
-let count = 0
+const message = 'Welcome in my Chat App'
 
 io.on('connection', (socket) => {
     console.log('New websocket connection')
-    socket.emit('countUpdated', count)
 
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
+    socket.emit('message', message)
+
+    socket.on('chatMsg', (msg)=> {
+        io.emit('message', msg)
     })
+
+    console.log('This is read only once')
+
+    // socket.emit('countUpdated', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+    // })
+
 })
 
 server.listen(port, () => {
