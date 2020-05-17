535d79daee811baf2d3fb414dfebe184cc45acd0
AdrianKubica <adrian.kubica.ak@gmail.com>
2020-05-17 14:08:25 +0200

    Form prepared for user actions

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,20 @@
 const socket = io()
 
-const redBox = document.querySelector('.red-box')
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $locationButton = document.querySelector('#send-location')
+const $redBox = document.querySelector('.red-box')
 
 const moveRedBox = () => {
     const red = parseInt(Math.random() * 255)
     const green = parseInt(Math.random() * 255)
     const blue = parseInt(Math.random() * 255)
-    const marginLeft = parseInt(getComputedStyle(redBox).marginLeft)
+    const marginLeft = parseInt(getComputedStyle($redBox).marginLeft)
 
-    redBox.style.backgroundColor = `rgb(${red},${green},${blue})`
-    redBox.style.marginLeft = `${marginLeft + 5}px`
+    $redBox.style.backgroundColor = `rgb(${red},${green},${blue})`
+    $redBox.style.marginLeft = `${marginLeft + 5}px`
 }
 
 socket.on('message', (message) => {
@@ -17,10 +22,15 @@ socket.on('message', (message) => {
     moveRedBox()
 })
 
-document.querySelector('#message-form').addEventListener('submit', (evt) => {
+$messageForm.addEventListener('submit', (evt) => {
     evt.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
     const message = evt.target.elements.message.value
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -29,13 +39,16 @@ document.querySelector('#message-form').addEventListener('submit', (evt) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
 
+    $locationButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', { latitude: position.coords.latitude, longitude: position.coords.longitude }, () => {
+            $locationButton.removeAttribute('disabled')
             console.log('Location shared')
         })
     })
